3652562fb0786b29bdb1172706907c6b66ae5cef
Alejandro Bohorquez <sergio.bohorquez@mail.escuelaing.edu.co>
2020-08-06 12:13:13 -0500

    NodeClassGeneral-Generalizing class to any type

--- src/main/java/edu/escuelaing/arep/calculadoraestadistica/linkedlist/LinkedList.java
+++ src/main/java/edu/escuelaing/arep/calculadoraestadistica/linkedlist/LinkedList.java
@@ -1,9 +1,10 @@
 package edu.escuelaing.arep.calculadoraestadistica.linkedlist;
 
+import java.beans.Expression;
 import java.math.BigDecimal;
+import java.util.Collection;
+import java.util.Iterator;
 
-public class LinkedList {
-
-
+public class LinkedList  {
 
 }
